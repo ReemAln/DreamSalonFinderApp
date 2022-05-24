87b7e5944d708f3e4bb4dfa8b7abf981ac73e3ac
Feffernoose <matt.penny82@gmail.com>
2022-05-24 17:42:18 -0400

    Map activity works, passes string but the activity does not recognize it yet Added the Facebook and Google login dependencies and fixed the strings

--- app/src/main/java/com/example/dreamsalonfinderapp/AllServices.java
+++ app/src/main/java/com/example/dreamsalonfinderapp/AllServices.java
@@ -2,13 +2,62 @@ package com.example.dreamsalonfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class AllServices extends AppCompatActivity {
+    Intent searchMapIntent, returnIntent;
+    String haircut, shampoo, styling, baseUrl, searchUrl = "", key = "key";
+    Boolean isHairCutChecked = false;
+    ImageButton haircutbtn;
+    Button searchBtn, returnBtn;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_services);
+        haircutbtn= findViewById(R.id.basic_service1_img);
+        searchBtn= findViewById(R.id.searchBtn);
+        returnBtn = findViewById(R.id.goBackButton);
+
+
+        baseUrl = "https://www.google.com/maps/search/";
+
+
+        haircutbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                isHairCutChecked = true;
+                haircut = "haircut";
+            }/* Once clicked the buttons set a string to the proper search param.
+               Then once the final button is pushed it appends the string to the base url
+               into the search url */
+
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchUrl = baseUrl + haircut;
+                searchMapIntent = new Intent(getApplicationContext(), MapsActivity.class);
+                searchMapIntent.putExtra(key, searchUrl);
+                startActivity(searchMapIntent);
+            }
+        });
+
+        returnBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(returnIntent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/dreamsalonfinderapp/MainActivity.java
+++ app/src/main/java/com/example/dreamsalonfinderapp/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.dreamsalonfinderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+/*
+        EditText editName = findViewById(R.id.edit_name);
+        EditText editPosition = findViewById(R.id.edit_position);
+
+        Button btn = findViewById(R.id.buttonSubmit);
+
+        DAOEMployee dao = new DAOEMployee();
+
+        btn.setOnClickListener(view -> {
+            Employee employee = new Employee(editName.getText().toString(), editPosition.getText().toString());
+
+            dao.add(employee).addOnSuccessListener(suc -> {
+                Toast.makeText(this, "This record is inserted", Toast.LENGTH_LONG).show();
+            }).addOnFailureListener(er -> {
+                Toast.makeText(this, "" + er.getMessage(), Toast.LENGTH_LONG).show();
+            });
+        });
+
+        btn.setOnClickListener(view -> {
+            // This is for update, need to add button as there is only one in the design now
+            HashMap<String, Object> hashMap = new HashMap<>();
+            hashMap.put("name", editName.getText().toString());
+            hashMap.put("position", editPosition.getText().toString());
+
+            // key comes from the database
+         /*   dao.update(key, hashMap).addOnSuccessListener(suc -> {
+                Toast.makeText(this, "This record is updated", Toast.LENGTH_LONG).show();
+            }).addOnFailureListener(er -> {
+                Toast.makeText(this, "" + er.getMessage(), Toast.LENGTH_LONG).show();
+            });
+        });*/
+        Button btn = findViewById(R.id.buttonSubmit);
+
+        btn.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), AllServices.class);
+            startActivity(intent);
+        });
+
+
     }
+
+
 }
